10795c9119b5979d00245f5a4b05fee8293450e5
Mourning-potato <ebowen35@ivytech.edu>
2025-04-30 15:57:05 -0400

    Create appointmentType.h
    Created appointment type header file

---
+++ appointmentType.h
@@ -0,0 +1,37 @@
+#ifndef appointment_test
+#define appointment_test
+#include <string>
+
+using namespace std;
+
+class appointmentType {
+public:
+	void changeDate(int y, int m, int d);
+	//changes the date to the variables in the parameters
+	void changeDescription(string desc);
+	//changes the description to what is in the parameter
+	appointmentType() {
+		year = 0;
+		month = 0;
+		day = 0;
+		description = "";
+	}
+	//default constructor
+	appointmentType(int y, int m, int d, string desc) {
+		year = y;
+		month = m;
+		day = d;
+		description = desc;
+	}
+	//constructor with parameters
+private:
+	int year;
+	int month;
+	int day;
+	int id;
+	string description;
+};
+
+
+
+#endif
