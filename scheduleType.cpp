d83495d6b17db7d788ae198f2fcfc4fe43f51e4a
eshwar-u <eshwar.umarengan@gmail.com>
2025-04-30 17:15:07 -0400

    created the appointment array in schedule type, and supporting functions.

--- scheduleType.cpp
+++ scheduleType.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
+#include "appointmentType.h"
 
+class ScheduleType {
+
+    //public variables, only included the array
+    public:
+        std::vector<appointmentType> appointmentArr;
+
+    //initializes the array, clears out all values (in case we need an empty array)
+    void initArray() {
+        appointmentArr.clear();
+    }
+
+    //adds one appointment to the array after the function is provided with the necessary values
+    void appendArray(int y, int m, int d, string desc) {
+        appointmentArr.push_back(appointmentType(y, m, d, desc));
+    }
+
+};
