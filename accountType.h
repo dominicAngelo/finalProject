5b641e1a74dd4ba81834a7cc1972cc24a46f6595
Mourning-potato <ebowen35@ivytech.edu>
2025-04-30 15:59:49 -0400

    Create accountType.h
    created the header file for account type
    
    also included some instructions for y'all in the comments if y'all want to get started on that

---
+++ accountType.h
@@ -0,0 +1,15 @@
+#ifndef account_test;
+#define account_test
+
+#include "appointmentType.h"
+#include "linkedListType.h"
+
+class accountType {
+public:
+
+protected:
+	appointmentType appointments[100];
+	//the above is a placeholder. I originally tried to use a linked list, but couldn't figure out a good way to have it return the needed data from a search. Try to impliment a dynamic array instead
+};
+
+#endif
