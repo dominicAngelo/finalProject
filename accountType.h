7f5023ca4a98b8a5381572cae0d92b039f5194af
Mourning-potato <ebowen35@ivytech.edu>
2025-04-30 16:26:24 -0400

    Update accountType.h
    added some file prototypes

--- accountType.h
+++ accountType.h
@@ -6,10 +6,17 @@
 
 class accountType {
 public:
-
+	void readData();
+	//reads the data from the file associated with the account
+	void addAppointment();
+	//adds an appointment to the appointments list
+	void deleteAppointment();
+	//remove an appointment from the appointments list
 protected:
 	appointmentType appointments[100];
 	//the above is a placeholder. I originally tried to use a linked list, but couldn't figure out a good way to have it return the needed data from a search. Try to impliment a dynamic array instead
+	string fname, lname;
+	//the name associated with the account. used to access it's file
 };
 
 #endif
