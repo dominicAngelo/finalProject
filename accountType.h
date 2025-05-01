a0c093821e7eec7e38d02a9b47d2f1c2b0326121
Mourning-potato <ebowen35@ivytech.edu>
2025-05-01 17:39:32 -0400

    Update accountType.h

--- accountType.h
+++ accountType.h
@@ -9,13 +9,18 @@ class accountType {
 public:
 	void readData();
 	//reads the data from the file associated with the account
+
+	void createAppointmentsBook();
+	//creates the account book array
+
 	void addAppointment();
 	//adds an appointment to the appointments list
+
 	void deleteAppointment();
 	//remove an appointment from the appointments list
 protected:
-	appointmentType appointments[100];
-	//the above is a placeholder. I originally tried to use a linked list, but couldn't figure out a good way to have it return the needed data from a search. Try to impliment a dynamic array instead
+	appointmentType *appointments;
+	//pointer holding the appointments array
 	string fname, lname;
 	//the name associated with the account. used to access it's file
 };
