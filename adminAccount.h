65b980da4e87793c55d94eb99ba5f13269c1e618
Mourning-potato <ebowen35@ivytech.edu>
2025-05-01 15:03:38 -0400

    Update adminAccount.h
    fixed the class implementation and added some method prototypes

--- adminAccount.h
+++ adminAccount.h
@@ -1,15 +1,19 @@
-#indef adminAccount
+#ifndef adminAccount
 #define adminAccount
 
 #include <iostream>
 #include "accountType.h"
 
-class adminType public accountType {
+class adminType: public accountType {
 public:
+	void loadAllData();
+	//loads all of the account data from their files into their classes
+	void addGroupAppointment();
+	//adds an appointment to several account's appointment books
 
-  
-  
 private:
 
 
 };
+
+#endif
