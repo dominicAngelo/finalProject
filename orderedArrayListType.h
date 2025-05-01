b0e4114e9d84f88c6b992ea8794537b4764f2229
Ethan Bowen <legostarswars2006@gmail.com>
2025-05-01 15:20:29 -0400

    Create orderedArrayListType.h

---
+++ orderedArrayListType.h
@@ -0,0 +1,28 @@
+class orderedArrayListType
+{
+public:
+    bool isEmpty() const;
+    bool isFull() const;
+    int listSize() const; 
+    int maxListSize() const; 
+    void print() const;
+    bool isItemAtEqual(int location, int item) const;
+    void insertAt(int location, int insertItem);
+    void insertEnd(int insertItem);
+    void removeAt(int location);
+    void retrieveAt(int location, int& retItem) const;
+    void replaceAt(int location, int repItem);
+    void clearList();
+    int seqSearch(int searchItem) const;
+    void insert(int insertItem);
+    void remove(int removeItem);
+    orderedArrayListType(int size = 100);
+    orderedArrayListType (const orderedArrayListType& otherList);
+    virtual ~orderedArrayListType();
+
+protected:
+    int *list;
+    int length;
+    int maxSize;
+
+};
