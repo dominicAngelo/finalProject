1f1141bdc94b01a0590d7a2389c6425374fc1b3e
Ethan Bowen <legostarswars2006@gmail.com>
2025-05-04 11:06:23 -0400

    Update arrayListType.h
    Rewrote the entire file

--- arrayListType.h
+++ arrayListType.h
@@ -1,29 +1,33 @@
 #ifndef array_list
 #define array_list
 
+template <class elemType>
 class arrayListType
 {
 public:
+    const arrayListType<elemType>&
+        operators=(const arrayListType<elemType>&);
     bool isEmpty() const;
     bool isFull() const;
-    int listSize() const; 
-    int maxListSize() const; 
+    int listSize() const;
+    int maxListSize() const;
     void print() const;
-    bool isItemAtEqual(int location, int item) const;
-    virtual void insertAt(int location, int insertItem) = 0;
-    virtual void insertEnd(int insertItem) = 0;
+    bool isItemAtEqual(int location, const elemType& item) const;
+    virtual void insertAt(int location, const elemType& insertItem) = 0;
+    virtual void insertEnd(const elemType& insertItem) = 0;
     void removeAt(int location);
-    void retrieveAt(int location, int& retItem) const;
-    virtual void replaceAt(int location, int repItem) = 0;
+    void retrieveAt(int location, const elemType& retItem) const;
+    virtual void replaceAt(int location, const elemType& repItem) const = 0;
     void clearList();
-    virtual int seqSearch(int searchItem) const = 0;
-    virtual void remove(int removeItem) = 0;
-    arrayListType(int size = 100);
-    arrayListType (const arrayListType& otherList);
+    virtual int seqSearch(const elemType& searchItem) const = 0;
+    virtual void remove(const elemType& removeItem) = 0;
+
+    arraListType(int size = 100);
+    arrayListType (const arrayListType<elemType>& otherList);
     virtual ~arrayListType();
 
 protected:
-    int *list;
+    elemType *list;
     int length;
     int maxSize;
 
