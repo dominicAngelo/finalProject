4e90db63fee431d2a877cba0940b4c29acdc2000
Mourning-potato <ebowen35@ivytech.edu>
2025-05-04 18:48:09 -0400

    Update accountType.h

--- accountType.h
+++ accountType.h
@@ -1,4 +1,4 @@
-#ifndef account_test;
+#ifndef account_test
 #define account_test
 using namespace std;
 
@@ -10,17 +10,11 @@ public:
 	void readData();
 	//reads the data from the file associated with the account
 
-	void createAppointmentsBook();
-	//creates the account book array
-
 	void addAppointment(int day, int month, int year, int hour, int minute, string desc);
 	//adds an appointment to the appointments list
 
-	void deleteAppointment();
+	void deleteAppointment(int index);
 	//remove an appointment from the appointments list
-
-	void loadData();
-	//loads account data
 
 	void normalMenu();
 	//added a public normal user menu
@@ -28,8 +22,15 @@ public:
 	string getLname() {
 		return lname;
 	}
+
+	accountType() {
+		fname = "";
+		lname = "";
+
+	}
+
 protected:
-	appointmentType* appointments;
+	orderedArrayListType<appointmentType> appointments;
 	//pointer holding the appointments array
 	string fname, lname;
 	//the name associated with the account. used to access it's file
