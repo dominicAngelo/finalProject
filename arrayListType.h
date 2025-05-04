9ae7d62de51000141a56ac2321502b521c71968f
Ethan Bowen <legostarswars2006@gmail.com>
2025-05-04 11:30:08 -0400

    Update arrayListType.h
    Added methods for the template functions

--- arrayListType.h
+++ arrayListType.h
@@ -32,4 +32,67 @@ protected:
     int maxSize;
 
 };
+
+template <class elemType>
+void arrayListType<elemType>::print()const
+{
+    for (int i = 0; i < length; i++)
+        cout << list[i] << " ";
+    cout << endl;
+}
+
+template <class elemType>
+bool arrayListType<elemType>::isItemAtEqual(int location, const elemType& item) const
+{
+    if (location < 0 || location >= length)
+    {
+        cout << "The location of the item to be removed it out of range." << endl;
+        return false;
+    }
+    else
+        return (list[location] == item);
+}
+
+template <class elemType>
+void arrayListType<elemType>::remove(int location)
+{
+    if (location < 0 || location >= length)
+        cout << "The location of the item to be removed it out of range." << endl;
+    else
+    {
+        for (int i = location; i < length - 1;; i++)
+            list[i] = list[i + 1];
+        length--;
+    }
+}
+
+template <class elemType>
+void arrayListType<elemType>::retrieveAt(int location, elemType& retItem) const
+{
+    if (location < 0 || location >= length)
+        cout << "The location of the item to be retrieved it out of range." << endl;
+    else
+        retItem = list[location];
+}
+
+template <class elemType>
+arrayListType<elemType>::arrayListType(int size)
+{
+    if (size <= 0)
+    {
+        cout << "The array size must be positive. Creating an array of the size 100. " << endl;
+    }
+    else
+        maxSize = size;
+    length = 0;
+
+    list = new elemType[maxSize];
+}
+
+template <class elemType>
+arrayListType<elemType>::~arrayListType()
+{
+    delete [] list;
+}
+
 #endif
