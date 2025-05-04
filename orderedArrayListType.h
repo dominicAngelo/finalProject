a88aebc4b4ad72800d6538131b7ff0c0119c542b
Ethan Bowen <legostarswars2006@gmail.com>
2025-05-04 10:39:08 -0400

    Update orderedArrayListType.h

--- orderedArrayListType.h
+++ orderedArrayListType.h
@@ -1,34 +1,41 @@
 #ifndef ordered_list
 #define ordered_list
 
-#include "arrayListType.h"
+template <class elemType>
 
 class orderedArrayListType: public arrayListType
 {
 public:
+    const orderedArrayListType<elemType>&
+        operator=(const orderedArrayListType<elemType>&);
+
     bool isEmpty() const;
     bool isFull() const;
     int listSize() const; 
     int maxListSize() const; 
     void print() const;
-    bool isItemAtEqual(int location, int item) const;
-    void insertAt(int location, int insertItem);
-    void insertEnd(int insertItem);
+    bool isItemAtEqual(int location, const elemType& item) const;
+    virtual void insertAt(int location, const elemType& insertItem);
+    virtual void insertEnd(const elemType& insertItem);
     void removeAt(int location);
-    void retrieveAt(int location, int& retItem) const;
-    void replaceAt(int location, int repItem);
+    void retrieveAt(int location, const elemType& retItem) const;
+    virtual void replaceAt(int location, int repItem);
     void clearList();
-    int seqSearch(int searchItem) const;
+    virtual int seqSearch(const elemType& searchItem) const;
     void insert(int insertItem);
-    void remove(int removeItem);
+    virtual void remove(const elemType& removeItem);
     orderedArrayListType(int size = 100);
-    orderedArrayListType (const orderedArrayListType& otherList);
+    orderedArrayListType (const orderedArrayListType<elemType>& otherList);
     virtual ~orderedArrayListType();
 
 protected:
-    int *list;
+    elemType *list;
     int length;
     int maxSize;
 
 };
+
+#include "orderedArrayListType.cpp"
+//This is so I don't have to rewrite the entire thing into here
+
 #endif
