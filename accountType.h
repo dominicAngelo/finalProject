49844a656fa2a2c161f7964631222ceb12eb19fc
Ethan Bowen <legostarswars2006@gmail.com>
2025-05-06 15:39:25 -0400

    Update accountType.h

--- accountType.h
+++ accountType.h
@@ -3,7 +3,6 @@
 using namespace std;
 
 #include "appointmentType.h"
-#include "orderedArrayListType.h"
 
 class accountType {
 public:
@@ -13,8 +12,21 @@ public:
 	void addAppointment(int day, int month, int year, int hour, int minute, string desc);
 	//adds an appointment to the appointments list
 
+
 	void deleteAppointment(int index);
 	//remove an appointment from the appointments list
+
+	void addAccount(string userName);
+	//adds an account name to the accounts array
+
+	void removeAccount(int index);
+	//removes an account name from the accounts array
+
+	void addPassword(string password);
+	//adds a password to the passwords array
+
+	void removePassword(int index);
+	//removes a password from the passwords array
 
 	void normalMenu();
 	//added a public normal user menu
@@ -30,8 +42,13 @@ public:
 	}
 
 protected:
-	orderedArrayListType<appointmentType> appointments;
+	appointmentType appointments[20];
 	//pointer holding the appointments array
+	appointmentType accounts[20];
+	//pointer holding the accounts array
+	appointmentType passwords[20];
+	//pointer holding the passwords array
+
 	string fname, lname;
 	//the name associated with the account. used to access it's file
 };
